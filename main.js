12db443a159217684a4422abe0f51c4a8102edd8
Kristen Bair <kristenmbair@gmail.com>
2020-10-22 10:51:43 -0600

    Add button functionality to display chosen message

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var affirmationRadioButton = document.querySelector('#aff');
+var mantraRadioButton = document.querySelector('#man');
+var generateButton = document.querySelector('#generate-button');
+var meditateImage = document.querySelector('img');
+var message = document.querySelector('#chosen-message');
 
 var mantras = [
   "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
@@ -32,3 +37,40 @@ var affirmations = [
   "I honor my body by trusting the signals that it sends me.",
   "I manifest perfect health by making smart choices."
 ];
+
+generateButton.addEventListener('click', chooseTypeOfSaying);
+
+function generateRandomArrayIndex(array) {
+  return Math.floor(Math.random() * array.length)
+}
+
+function chooseTypeOfSaying() {
+  //if aff radio button call choose aff function
+  if(affirmationRadioButton.value) {
+    chooseAffirmation();
+  } else if(mantraRadioButton.value) {
+    chooseMantra();
+  }
+  //if mantra radio button call choose man function
+  //else return error or alert "Please select a type of message."
+}
+
+function chooseMantra() {
+ var randomMantra = mantras[generateRandomArrayIndex(mantras)];
+ displayText(randomMantra);
+ }
+//pull randomMantra out of array to prevent duplicates
+//if statement - if mantras is empty .....?
+
+function chooseAffirmation() {
+  var randomAffirmation = affirmations[generateRandomArrayIndex(affirmations)];
+  displayText(randomAffirmation);
+}
+
+function displayText(typeOfSaying) {
+  meditateImage.classList.add("hidden");
+  message.classList.remove("hidden");
+  message.innerText = typeOfSaying;
+  // input the typeOfSaying parameter into the inner HTML of the second div
+  // hide the image
+}
